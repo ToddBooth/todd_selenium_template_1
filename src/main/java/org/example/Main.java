6842c0966d22f013507f7f65487166fc028f209d
ToddBooth <ToddBooth@users.noreply.github.com>
2022-05-13 15:39:18 +0200

    commit_3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,13 @@
 package org.example;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Main {
 
+    // Chrome
     static String DRIVER_TYPE = "webdriver.chrome.driver";
     static String DRIVER_FILE = "./drivers/chromedriver_v101.exe";
 
@@ -15,37 +16,48 @@ public class Main {
 
         // Chrome
         System.setProperty(DRIVER_TYPE, DRIVER_FILE);
-        ChromeDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("incognito");
+        options.addArguments("start-maximized");
+        ChromeDriver driver = new ChromeDriver(options);
 
         // Maximizes window to show the full content (the "Try for free" button)
         driver.manage().window().maximize();
 
-        driver.get("https://actitime.com");
+        // Yahoo
+        driver.get("https://yahoo.com");
         System.out.println(driver.getTitle());
+        driver.manage().deleteAllCookies();
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        Thread.sleep(200);
+        String xpath = "/html//div[@id='consent-page']/div/div//form//button[@name='agree']";
+        driver.findElement(By.xpath(xpath)).click();
 
-        String text = "Try Free";
-        driver.findElement(By.linkText(text)).click();
+        Thread.sleep(200);
+        xpath = "//div[@innertext='Logga in']";
+        // driver.findElement(By.xpath(xpath)).click();
+        String css = "._yb_trcuk";
+        driver.findElement(By.cssSelector(css)).click();
 
-        Thread.sleep(300);
+        Thread.sleep(200);
+        xpath = "//html[@id='Stencil']//a[@id='createacc']";
+        driver.findElement(By.xpath(xpath)).click();
 
-        // First Name
-        driver.findElement(By.xpath("/html//input[@id='First Name']")).sendKeys("Todd");
+        Thread.sleep(200);
+        xpath = "//html[@id='Stencil']//input[@id='usernamereg-firstName']";
+        driver.findElement(By.xpath(xpath)).sendKeys("John");
 
-        // Last Name
-        String LAST_NAME = "Last Name";
-        driver.findElement(By.id(LAST_NAME)).sendKeys("Booth");
+        Thread.sleep(200);
+        xpath = "//html[@id='Stencil']//input[@id='usernamereg-lastName']";
+        driver.findElement(By.xpath(xpath)).sendKeys("Doe" +
+                Keys.TAB + "hejsan2477" +
+                Keys.TAB + "password123" +
+                Keys.TAB + "1998");
 
-        // Email
-        driver.findElement(By.cssSelector("input#Email")).sendKeys("SomeEmail@Gmail.Com");
-
-        // Company
-        String COMPANY = "Company";
-        driver.findElement(By.id(COMPANY)).sendKeys("Luleå University of Technology");
-
-        // Submit button
-        driver.findElement(By.xpath("/html//a[@id='confirm-button']")).click();
+        Thread.sleep(200);
+        xpath = "//html[@id='Stencil']//button[@id='reg-submit-button']";
+        // I disabled the following so that I don't actually create a Yahoo account
+        // driver.findElement(By.xpath(xpath)).click();
 
         // Clean up manually
         driver.manage().deleteAllCookies();
